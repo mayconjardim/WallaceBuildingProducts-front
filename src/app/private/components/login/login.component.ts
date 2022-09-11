1b90556f12adea848f59ae053e9f159d86c0ea13
Maycon Jardim <mayconsmill@gmail.com>
2022-09-11 17:46:19 -0300

    Validação de forms

--- src/app/private/components/login/login.component.ts
+++ src/app/private/components/login/login.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormControl } from '@angular/forms';
+import { faSearchengin } from '@fortawesome/free-brands-svg-icons';
+import { Credentials } from 'src/app/private/models/credentials';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  creds: Credentials = {
+    name: '',
+    password: '',
+  };
 
-  constructor() { }
+  name = new FormControl(null, Validators.required);
+  password = new FormControl(null, Validators.minLength(3));
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  validForms(): boolean {
+    if (this.name.valid && this.password.valid) {
+      return true;
+    } else {
+      return false;
+    }
   }
-
 }
